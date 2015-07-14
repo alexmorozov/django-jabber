f007efaa185434170f4c98f71a9afb1b930baacf
Alex Morozov <morozov@podmoskovie.info>
2015-07-14 11:35:29 +0300

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(requirements_file):
 
 setuptools.setup(
     name="django-jabber",
-    version="0.2.0",
+    version="0.2.1",
     url="https://github.com/alexmorozov/django-jabber",
 
     author="Alex Morozov",
